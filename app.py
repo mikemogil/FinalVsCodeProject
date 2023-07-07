65096591700a19873b0f7acc11a2a2b0dc8cc68d
= <=>
2023-07-07 13:56:07 -0400

    7-7-2023

--- app.py
+++ app.py
@@ -29,26 +29,26 @@ def submit_form():
     return redirect(url_for('success', file= datainfo[2], part_number=part_number, revision_number=revision_number, dropdown=dropdown_value))
 
 
+    
+
 
 
 @app.route('/success', methods=['GET', 'POST'])
 def success():
 
-    print(list(request.args.keys()), "**************************hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh**********************")
-    part_number = request.args.get('part_number')
-    revision_number = request.args.get('revision_number')
-    dropdown_value = request.args.get('dropdown')
-    part_number = 'DRW-02446-01'
-    revision_number = 'RAB'
+    
     if request.method == 'POST':
         # Get the edited data from the form
+        part_number = request.form.get('part_number')
+        revision_number = request.form.get('revision_number')
+        dropdown_value = request.form.get('dropdown')
+        print(part_number, revision_number)
         edited_dataPRB = request.form.getlist('edited_dataPRB')
         edited_dataJB = request.form.getlist('edited_dataJB')
-
-       
         # Load the Excel file
         workbook_pathPRB = f'C:\FinalVsCodeProject\PRB,{part_number},{revision_number}.xlsx'
         workbook_pathJB = f'C:\FinalVsCodeProject\JB,{part_number},{revision_number}.xlsx' 
+        
         workbookPRB = openpyxl.load_workbook(workbook_pathPRB)
         workbookJB = openpyxl.load_workbook(workbook_pathJB)
         sheetPRB = workbookPRB.active
@@ -59,6 +59,7 @@ def success():
             row_number = i // sheetPRB.max_column + 2  # Add 2 to skip the header row
             col_number = i % sheetPRB.max_column + 1
             sheetPRB.cell(row=row_number, column=col_number).value = value
+            
 
         for i, value in enumerate(edited_dataJB):
             row_number = i // sheetJB.max_column + 2  # Add 2 to skip the header row
@@ -70,11 +71,14 @@ def success():
         workbookJB.save(workbook_pathJB)
 
         # Redirect to the success page
-        return redirect('/success')
+        return 'An email Has been sent to Mark Dieckman.'
     else:
         # Get the initial data from the Excel sheet
+        part_number = request.args.get('part_number')
+        revision_number = request.args.get('revision_number')
+        dropdown_value = request.args.get('dropdown')
         
-
+        
         workbook_pathPRB = f'C:\FinalVsCodeProject\PRB,{part_number},{revision_number}.xlsx'
         workbook_pathJB = f'C:\FinalVsCodeProject\JB,{part_number},{revision_number}.xlsx' 
 
